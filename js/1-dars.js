c7172f8fa1db233161be4d2d93442dd7794a434b
WhiteHakerMe <140898334+WhiteHakerMe@users.noreply.github.com>
2023-11-19 00:25:20 +0500

    main
    js-darslar frontend it-park

---
+++ js/1-dars.js
@@ -0,0 +1,37 @@
+// let numer = 30
+// let numer1 = 20
+// let numer2 = 10
+// let qabul = prompt("brinchi son kriting")
+// let qabul1 = prompt("ikkinch son krit")
+// javob = Number(Number(qabul) + Number(qabul1))
+// J = Number(numer2 + numer2)
+// let example_one = prompt(qabul + " + " + qabul1 + " shuni endi O'zing yech")
+// let example_two = prompt("10 + 10")
+// let pramry = prompt("10 + 20")
+// let Javob2 = Number(numer1 + numer2)
+
+// if (pramry == Javob2) {
+//     console.log("1chi savolga: siz to'g'ri javob berdingz")
+//     alert("1chi savolga: siz to'g'ri javob berdingz")
+// } else {
+//     console.log("1chi savolga: siz noto'g'ri javob berdingiz To'g'ri javob: " + Javob2)
+//     alert("1chi savolga: siz noto'g'ri javob berdingiz To'g'ri javob: " + Javob2)
+// }
+
+// if (example_one == javob) {
+//     console.log("2chi savolga: sizni javobingiz to'g'ri")
+//     alert("2chi savolga: sizni javobingiz to'g'ri")
+// } else {
+//     console.log("3chi savolga: siz noto'g'ri javob berdingz " + javob + " to'g'rijavob " + " uzingni misolingni uzing yecholmading")
+//     alert("3chi savolga: siz noto'g'ri javob berdingz " + javob + " to'g'rijavob " + " uzingni misolingni uzing yecholmading")
+// }
+
+// if (example_two == J) {
+//     console.log(" siz to'g'ri javob berdingz tabriklayman")
+//     alert("siz to'g'ri javob berdingz tabriklayman")
+// } else {
+//     console.log("sizni javobingz xato " + "To'g'ri Javob = " + J)
+//     alert("sizni javobingz xato " + "To'g'ri Javob = " + J)
+// }
+// let yozma = ["Lorem, ipsum dolor sit amet consectetur dolor elit. Omnis non molestias accusantium veniam, dolar velit unde!"]
+// console.log(yozma.splice("").reduceWith("dolor"));
---
+++ js/2-dars.js
@@ -0,0 +1,12 @@
+// function
+
+
+// function FirstNum (num1, num2) {
+//     console.log(num1 + num2);
+// }
+// FirstNum(+prompt("brinchi sonni kriting"), +prompt("ikki sonni kriting")) // Buyerda Number == sonlarga funksia tuzilgan
+
+function contextStr (text) {
+    console.log(text.split("").reverse("").join("").toLowerCase());
+}
+contextStr(prompt("Matin kriting"))
